6c5262db4e9069c3b64f163d4528627d8f104f89
Manuella Maia Lopes <manuellamaiamana07@gmail.com>
2026-07-18 21:32:54 -0300

    refactor(js): reorganiza lógica da interfac

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,40 +1,39 @@
 export function mostrarMensagemCadastro(mensagem, tipo) {
-    const elemento = document.getElementById('infoMessage');
+  const elemento = document.getElementById("infoMessage");
 
-    elemento.classList.remove('sucesso','erro'); 
-    
-    if(tipo == 'sucesso'){
-        elemento.classList.add('sucesso')
-    }else if(tipo == 'erro'){
-        elemento.classList.add('erro')
-    }
+  elemento.classList.remove("sucesso", "erro");
 
-    elemento.textContent = mensagem
+  if (tipo == "sucesso") {
+    elemento.classList.add("sucesso");
+  } else if (tipo == "erro") {
+    elemento.classList.add("erro");
+  }
 
-    setTimeout(() => {
-        elemento.textContent = ""
-        elemento.classList.remove('sucesso','erro');
+  elemento.textContent = mensagem;
 
-    }, 3000);
+  setTimeout(() => {
+    elemento.textContent = "";
+    elemento.classList.remove("sucesso", "erro");
+  }, 3000);
 }
 
 export function limparFormularioCadastro() {
-    // TODO: Pegar o form com id="missionForm"
-    // Chamar form.reset() para limpar todos os campos
-    const formulario = document.getElementById('missionForm');
+  // TODO: Pegar o form com id="missionForm"
+  // Chamar form.reset() para limpar todos os campos
+  const formulario = document.getElementById("missionForm");
 
-    if(formulario){
-        formulario.reset();
-    }
+  if (formulario) {
+    formulario.reset();
+  }
 }
 
 export function mostrarLoadingTabela() {
-    // TODO: Pegar tbody com id="missionsBody"
-    // Definir innerHTML com uma linha dizendo "Carregando..."
+  // TODO: Pegar tbody com id="missionsBody"
+  // Definir innerHTML com uma linha dizendo "Carregando..."
 
-    const tbody = document.getElementById('missionsBody');
+  const tbody = document.getElementById("missionsBody");
 
-    tbody.innerHTML = `
+  tbody.innerHTML = `
         <tr>
             <td colspan="7" style="text-align: center;">
                 Carregando missões espaciais...
@@ -44,15 +43,15 @@ export function mostrarLoadingTabela() {
 }
 
 export function renderizarMissoes(missoes) {
-    const tbody = document.getElementById('missionsBody');
-    tbody.innerHTML = "";
+  const tbody = document.getElementById("missionsBody");
+  tbody.innerHTML = "";
 
-    if (missoes.length === 0) {
-        tbody.innerHTML = `<tr><td colspan="7">Nenhuma missão cadastrada</td></tr>`;
-        return; 
-    }
+  if (missoes.length === 0) {
+    tbody.innerHTML = `<tr><td colspan="7">Nenhuma missão cadastrada</td></tr>`;
+    return;
+  }
 
-    missoes.forEach(mission => {
+  missoes.forEach((mission) => {
     const linha = `
         <tr>
             <td>${mission.nome}</td>
@@ -62,22 +61,24 @@ export function renderizarMissoes(missoes) {
             <td>${mission.status}</td>
             <td>${mission.durations}</td>
             <td>
-                <button class="button-small edit btn-editar" data-id="${mission.id}">Editar</button>
-                <button class="button-small delete btn-deletar" data-id="${mission.id}">Deletar</button>
+                <div class="actions">
+                    <button class="button-small edit btn-editar" data-id="${mission.id}">Editar</button>
+                    <button class="button-small delete btn-deletar" data-id="${mission.id}">Deletar</button>
+                </div>
             </td>
         </tr>`;
-    
-        tbody.innerHTML += linha; 
-    });
+
+    tbody.innerHTML += linha;
+  });
 }
 
 export function mostrarErroTabela(mensagem) {
-    // TODO: Pegar tbody com id="missionsBody"
-    // Definir innerHTML com uma linha dizendo o erro
+  // TODO: Pegar tbody com id="missionsBody"
+  // Definir innerHTML com uma linha dizendo o erro
 
-    const tbody = document.getElementById('missionsBody');
+  const tbody = document.getElementById("missionsBody");
 
-    tbody.innerHTML = `
+  tbody.innerHTML = `
         <tr>
             <td colspan="7" style="text-align: center;">
                 ${mensagem}
@@ -87,54 +88,53 @@ export function mostrarErroTabela(mensagem) {
 }
 
 export function mostrarPainelEdicao(missao) {
-    const div = document.getElementById('editPanel');
-    const info = document.getElementById('editInfo');
+  const div = document.getElementById("editPanel");
+  const info = document.getElementById("editInfo");
 
-    div.classList.remove('hidden');
+  div.classList.remove("hidden");
 
-    document.getElementById('editNome').value = missao.nome
-    document.getElementById('editCrew').value = missao.crew
-    document.getElementById('editSpacecraft').value = missao.spacecraft
-    document.getElementById('editDestinations').value = missao.destinations
-    document.getElementById('editStatus').value = missao.status
-    document.getElementById('editDurations').value = missao.durations
+  document.getElementById("editNome").value = missao.nome;
+  document.getElementById("editCrew").value = missao.crew;
+  document.getElementById("editSpacecraft").value = missao.spacecraft;
+  document.getElementById("editDestinations").value = missao.destinations;
+  document.getElementById("editStatus").value = missao.status;
+  document.getElementById("editDurations").value = missao.durations;
 
-    info.textContent = ""
+  info.textContent = "";
 }
 
 export function esconderPainelEdicao() {
-    // TODO: Pegar div com id="editPanel"
-    // Adicionar classe 'hidden' para esconder
-    const div = document.getElementById('editPanel');
-    div.classList.add('hidden')
+  // TODO: Pegar div com id="editPanel"
+  // Adicionar classe 'hidden' para esconder
+  const div = document.getElementById("editPanel");
+  div.classList.add("hidden");
 }
 
 export function mostrarMensagemEdicao(mensagem, tipo) {
-    // TODO: Pegar div com id="editInfo"
-    // Definir textContent com a mensagem
-    // Adicionar/remover classes CSS baseadas no tipo (sucesso/erro)
-    const div = document.getElementById('editInfo');
+  // TODO: Pegar div com id="editInfo"
+  // Definir textContent com a mensagem
+  // Adicionar/remover classes CSS baseadas no tipo (sucesso/erro)
+  const div = document.getElementById("editInfo");
 
-    div.classList.remove('sucesso','erro');
+  div.classList.remove("sucesso", "erro");
 
-    if(tipo == 'sucesso'){
-        div.classList.add('sucesso')
-    }else if(tipo == 'erro'){
-        div.classList.add('erro')
-    }
+  if (tipo == "sucesso") {
+    div.classList.add("sucesso");
+  } else if (tipo == "erro") {
+    div.classList.add("erro");
+  }
 
-    div.textContent = mensagem
+  div.textContent = mensagem;
 
-    setTimeout(() => {
-        div.textContent = ""
-        div.classList.remove('sucesso','erro');
-
-    }, 3000);
+  setTimeout(() => {
+    div.textContent = "";
+    div.classList.remove("sucesso", "erro");
+  }, 3000);
 }
 
 export function limparFormularioEdicao() {
-    // TODO: Pegar form com id="editForm"
-    // Chamar form.reset()
-    const form = document.getElementById('editForm')
-    form.reset();
+  // TODO: Pegar form com id="editForm"
+  // Chamar form.reset()
+  const form = document.getElementById("editForm");
+  form.reset();
 }
