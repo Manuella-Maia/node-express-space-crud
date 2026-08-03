ab2e56d27bdff89d11e985241fd2263db1d65e85
Manuella Maia Lopes <manuellamaiamana07@gmail.com>
2026-08-02 21:06:13 -0300

    feat(js): adiciona validação de id

--- src/services/validation.js
+++ src/services/validation.js
@@ -1,78 +1,100 @@
-import {insertMissions, selectMissions, selectMissionsById, updateMission, deleteMission} from '../models/missionModel.js';
+import {
+  insertMissions,
+  selectMissions,
+  selectMissionsById,
+  updateMission,
+  deleteMission,
+} from "../models/missionModel.js";
 
 export async function validarCampos(dados) {
+  const { nome, crew, spacecraft, destinations, status, durations } = dados;
 
-    const {nome, crew, spacecraft, destinations, status, durations} = dados;
+  if (!nome || !crew || !spacecraft || !destinations || !status || !durations) {
+    throw new Error("Dados invalidos ou ausentes !");
+  }
 
-    if(!nome || !crew || !spacecraft || !destinations || !status || !durations){
-        throw new Error('Dados invalidos ou ausentes !');
-    };
+  const crewNum = parseInt(crew, 10);
+  if (isNaN(crewNum)) {
+    throw new Error("O campo crew deve ser um número válido!");
+  }
 
-    const crewNum = parseInt(crew, 10);
-    if (isNaN(crewNum)) {
-        throw new Error('O campo crew deve ser um número válido!');
-    }
+  if (crewNum <= 0) {
+    throw new Error("Numero de tripulantes invalido !");
+  }
 
-    if(crewNum <= 0){
-        throw new Error('Numero de tripulantes invalido !');
-    };
+  dados.crew = crewNum;
+}
 
-    dados.crew = crewNum;
-};
+export async function validarID(id) {
+  if (id === undefined || id === null) {
+    throw new Error("Id não informado");
+  }
+
+  const idNumber = parseInt(id, 10);
+
+  if (isNaN(idNumber)) {
+    throw new Error("O id não é um número");
+  }
+
+  if (idNumber < 1) {
+    throw new Error("Id inválido: número negativo ou igual a 0");
+  }
+
+  return idNumber;
+}
 
 export async function cadastrarMissao(dados) {
+  await validarCampos(dados);
 
-    await validarCampos(dados);
+  const missaoCriada = await insertMissions(dados);
 
-    const missaoCriada = await insertMissions(dados);
-
-    return missaoCriada;
-};
+  return missaoCriada;
+}
 
 export async function litarMissoes() {
+  const listedMissions = await selectMissions();
 
-    const listedMissions = await selectMissions();
-
-    return listedMissions;
-};
+  return listedMissions;
+}
 
 export async function litarMissaoId(id) {
+  const idValid = await validarID(id);
 
-    const missionById = await selectMissionsById(id);
+  const missionById = await selectMissionsById(idValid);
 
-    if(!missionById){
-        throw new Error(`Missão com ID ${id} não encontrada.`);
-    }
+  if (!missionById) {
+    throw new Error(`Missão com ID ${idValid} não encontrada.`);
+  }
 
-    return missionById;
-};
+  return missionById;
+}
 
-export async function atualizarMission(dados,id) {
+export async function atualizarMission(dados, id) {
+  await validarCampos(dados);
 
-    await validarCampos(dados);
+  const idValid = await validarID(id);
 
-    const mission = await updateMission(dados,id);
+  const mission = await updateMission(dados, idValid);
 
-    if(mission.changes == 0){
-        throw new Error('Missions não encontrada ! Id não existe no bd');
-    };
+  if (mission.changes == 0) {
+    throw new Error("Missions não encontrada ! Id não existe no bd");
+  }
 
-    return mission;
-};
+  return mission;
+}
 
 export async function deletarMissaoId(id) {
+  const missionById = await selectMissionsById(id);
 
-    const missionById = await selectMissionsById(id);
+  if (!missionById) {
+    throw new Error(`Missão com ID ${id} não encontrada.`);
+  }
 
-    if(!missionById){
-        throw new Error(`Missão com ID ${id} não encontrada.`);
-    }
+  const missionDeleted = await deleteMission(id);
 
-    const missionDeleted = await deleteMission(id);
+  if (missionDeleted.changes == 0) {
+    throw new Error("A Missão não foi deletada corretamente !");
+  }
 
-    if(missionDeleted.changes == 0){
-         throw new Error('A Missão não foi deletada corretamente !');
-    };
-
-    return missionById;
-};
+  return missionById;
+}
